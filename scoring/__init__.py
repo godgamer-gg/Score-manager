c6f39e3f5d757e8c6db65bf537a0d2143789043d
Nick <nickarmstrong888@gmail.com>
2025-04-17 22:50:07 -0700

    trying this to fix remote build

--- scoring/__init__.py
+++ scoring/__init__.py
@@ -1,4 +1,4 @@
-from .scoreManager import *
-import os, sys
+# from .scoreManager import *
+# import os, sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
